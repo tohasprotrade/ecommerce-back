5d1e70fe0b2f20b481877f056f561b8855cdbd36
fuad-dev-portfolio <abdullahalfuad.dev@gmail.com>
2026-03-16 22:06:03 +0600

    fixes cors origin issue

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ import orderRouter from './route/order.route.js'
 const app = express()
 app.use(cors({
     credentials : true,
-    origin : process.env.FRONTEND_URL || true
+    origin : true
 }))
 app.use(express.json())
 app.use(cookieParser())
